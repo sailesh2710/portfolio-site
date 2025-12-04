23aa33ff2d7d6850d0758357bcdf30c7101a8c87
sailesh2710 <saileshkumar2710@gmail.com>
2025-12-04 08:25:02 -0800

    Add missing artifact_bucket_access policy and fix IAM attachments

--- infra/elasticbeanstalk.tf
+++ infra/elasticbeanstalk.tf
@@ -1,4 +1,90 @@
 # ---------------------------------------
+# Elastic Beanstalk Service & EC2 Roles
+# ---------------------------------------
+
+# EB Service Role
+resource "aws_iam_role" "eb_service_role" {
+  name = "aws-elasticbeanstalk-service-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "elasticbeanstalk.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "eb_service_role_extra" {
+  name = "eb-service-extra-permissions"
+  role = aws_iam_role.eb_service_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ec2:DescribeLaunchTemplates",
+          "ec2:DescribeLaunchTemplateVersions",
+          "ec2:DescribeInstances",
+          "ec2:DescribeInstanceTypes",
+          "ec2:DescribeImages",
+          "ec2:DescribeAccountAttributes",
+          "ec2:DescribeSecurityGroups",
+          "ec2:DescribeVpcs",
+          "ec2:DescribeSubnets",
+          "ec2:DescribeNetworkInterfaces",
+          "ec2:DescribeKeyPairs",
+          "ec2:DescribePlacementGroups"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+
+resource "aws_iam_role_policy_attachment" "eb_service_role_managed2" {
+  role       = aws_iam_role.eb_service_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSElasticBeanstalkService"
+}
+
+# EB EC2 Instance Role
+resource "aws_iam_role" "eb_ec2_role" {
+  name = "aws-elasticbeanstalk-ec2-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+
+resource "aws_iam_role_policy_attachment" "eb_ec2_role_managed2" {
+  role       = aws_iam_role.eb_ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSElasticBeanstalkWorkerTier"
+}
+
+resource "aws_iam_instance_profile" "eb_instance_profile" {
+  name = "aws-elasticbeanstalk-ec2-role"
+  role = aws_iam_role.eb_ec2_role.name
+}
+
+# ---------------------------------------
 # Elastic Beanstalk Application
 # ---------------------------------------
 resource "aws_elastic_beanstalk_application" "portfolio_app" {
@@ -18,14 +104,14 @@ resource "aws_elastic_beanstalk_environment" "portfolio_env" {
   setting {
     namespace = "aws:elasticbeanstalk:environment"
     name      = "ServiceRole"
-    value     = "aws-elasticbeanstalk-service-role"
+    value     = aws_iam_role.eb_service_role.name
   }
 
   # EB EC2 Instance Profile
   setting {
     namespace = "aws:autoscaling:launchconfiguration"
     name      = "IamInstanceProfile"
-    value     = "aws-elasticbeanstalk-ec2-role"
+    value     = aws_iam_instance_profile.eb_instance_profile.name
   }
 
   # ---------------------------------------
@@ -106,44 +192,136 @@ resource "aws_iam_policy" "codepipeline_eb_policy" {
   description = "Allow CodePipeline to create EB application versions and update environment"
 
   policy = jsonencode({
-    Version = "2012-10-17"
+  Version = "2012-10-17"
+  Statement = [
+    {
+      Effect = "Allow"
+      Action = [
+        "cloudformation:DescribeStacks",
+        "cloudformation:DescribeStackResource",
+        "cloudformation:DescribeStackEvents",
+        "cloudformation:GetTemplate",
+        "cloudformation:UpdateStack",
+        "cloudformation:CreateChangeSet",
+        "cloudformation:DescribeChangeSet",
+        "cloudformation:ExecuteChangeSet"
+      ]
+      Resource = "*"
+    },
+    {
+      Effect = "Allow"
+      Action = [
+        "elasticbeanstalk:*",
+        "s3:GetObject",
+        "s3:PutObject",
+        "s3:ListBucket"
+      ]
+      Resource = "*"
+    },
+    {
+      Effect = "Allow"
+      Action = [
+        "autoscaling:UpdateAutoScalingGroup",
+        "autoscaling:CreateAutoScalingGroup",
+        "autoscaling:PutScalingPolicy",
+        "autoscaling:DescribeAutoScalingGroups",
+        "autoscaling:DescribePolicies",
+        "autoscaling:DescribeScalingActivities",
+        "autoscaling:DescribeLaunchConfigurations",
+        "autoscaling:CreateLaunchConfiguration",
+        "autoscaling:DeleteLaunchConfiguration",
+        "autoscaling:CreateOrUpdateTags",
+        "ec2:RunInstances",
+        "ec2:TerminateInstances",
+        "ec2:CreateLaunchTemplate",
+        "ec2:CreateLaunchTemplateVersion",
+        "ec2:ModifyLaunchTemplate",
+        "ec2:DeleteLaunchTemplate",
+        "ec2:DescribeLaunchTemplates",
+        "ec2:DescribeLaunchTemplateVersions",
+        "ec2:GetLaunchTemplateData",
+        "ec2:DescribeInstances",
+        "ec2:DescribeInstanceTypes",
+        "ec2:DescribeImages",
+        "ec2:DescribeAccountAttributes",
+        "ec2:DescribeSecurityGroups",
+        "ec2:DescribeVpcs",
+        "ec2:DescribeSubnets",
+        "ec2:DescribeNetworkInterfaces",
+        "ec2:CreateTags",
+        "ec2:DescribeKeyPairs",
+        "ec2:DescribePlacementGroups"
+      ]
+      Resource = "*"
+    }
+  ]
+})
+}
+
+resource "aws_iam_role_policy_attachment" "codepipeline_eb_policy_attach" {
+  role       = aws_iam_role.codepipeline_role.name
+  policy_arn = aws_iam_policy.codepipeline_eb_policy.arn
+}
+
+
+resource "aws_iam_role_policy_attachment" "artifact_bucket_attach_pipeline" {
+  role       = aws_iam_role.codepipeline_role.name
+  policy_arn = aws_iam_policy.artifact_bucket_access.arn
+}
+
+resource "aws_iam_role_policy_attachment" "artifact_bucket_attach_codebuild" {
+  role       = aws_iam_role.codebuild_role.name
+  policy_arn = aws_iam_policy.artifact_bucket_access.arn
+}
+
+resource "aws_iam_policy" "kms_artifact_access" {
+  name = "portfolio-kms-artifact-access"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
     Statement = [
       {
-        Effect = "Allow"
+        Effect = "Allow",
         Action = [
-          "elasticbeanstalk:CreateApplicationVersion",
-          "elasticbeanstalk:DescribeApplicationVersions",
-          "elasticbeanstalk:DescribeEnvironments",
-          "elasticbeanstalk:UpdateEnvironment"
-        ]
-        Resource = "*"
-      },
-      {
-        Effect = "Allow"
-        Action = [
-          "s3:GetObject",
-          "s3:GetObjectVersion"
-        ]
-        # Let EB pull the build artifact from your artifact bucket
-        Resource = "${aws_s3_bucket.artifact_bucket.arn}/*"
-      },
-      {
-        Effect = "Allow"
-        Action = [
-          "cloudformation:GetTemplate",
-          "cloudformation:DescribeStacks",
-          "cloudformation:DescribeStackResources",
-          "cloudformation:ListStackResources"
-        ]
-        # Allow CodePipeline to read the Elastic Beanstalk backing CloudFormation stacks
+          "kms:Encrypt",
+          "kms:Decrypt",
+          "kms:ReEncrypt*",
+          "kms:GenerateDataKey*",
+          "kms:DescribeKey"
+        ],
         Resource = "*"
       }
     ]
   })
 }
 
-resource "aws_iam_policy_attachment" "codepipeline_eb_policy_attach" {
-  name       = "codepipeline-eb-policy-attach"
-  roles      = [aws_iam_role.codepipeline_role.name]
-  policy_arn = aws_iam_policy.codepipeline_eb_policy.arn
+resource "aws_iam_role_policy_attachment" "kms_access_pipeline" {
+  role       = aws_iam_role.codepipeline_role.name
+  policy_arn = aws_iam_policy.kms_artifact_access.arn
+}
+
+resource "aws_iam_role_policy_attachment" "kms_access_codebuild" {
+  role       = aws_iam_role.codebuild_role.name
+  policy_arn = aws_iam_policy.kms_artifact_access.arn
+}
+
+# Policy to allow CodePipeline + CodeBuild to access artifact bucket
+resource "aws_iam_policy" "artifact_bucket_access" {
+  name        = "portfolio-artifact-bucket-access"
+  description = "Allows access to the S3 artifact bucket for builds and deployments"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:ListBucket"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
 }
--- infra/main.tf
+++ infra/main.tf
@@ -34,53 +34,6 @@ resource "aws_iam_policy_attachment" "codepipeline_policy_attach" {
   policy_arn = "arn:aws:iam::aws:policy/AWSCodePipeline_FullAccess"
 }
 
-# Elastic Beanstalk service role
-resource "aws_iam_role" "eb_service_role" {
-  name = "aws-elasticbeanstalk-service-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Effect = "Allow",
-      Principal = {
-        Service = "elasticbeanstalk.amazonaws.com"
-      },
-      Action = "sts:AssumeRole"
-    }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "eb_service_role_attach" {
-  role       = aws_iam_role.eb_service_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSElasticBeanstalkEnhancedHealth"
-}
-
-# EC2 instance profile for EB
-resource "aws_iam_role" "eb_ec2_role" {
-  name = "aws-elasticbeanstalk-ec2-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Effect = "Allow",
-      Principal = {
-        Service = "ec2.amazonaws.com"
-      },
-      Action = "sts:AssumeRole"
-    }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "eb_ec2_role_attach" {
-  role       = aws_iam_role.eb_ec2_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AWSElasticBeanstalkWebTier"
-}
-
-resource "aws_iam_instance_profile" "eb_instance_profile" {
-  name = "aws-elasticbeanstalk-ec2-role"
-  role = aws_iam_role.eb_ec2_role.name
-}
-
 resource "aws_iam_role" "codebuild_role" {
   name = "portfolio-codebuild-role"
 
